c7b24cb26cf055623f7d7d00db7a9c54e59560b1
Ibrahim Ismail <i.ismail@neopplanet.com>
2022-04-18 17:38:20 +0200

    Handle Women Classification

--- App.py
+++ App.py
@@ -9,21 +9,21 @@ import io
 import numpy as np
 import sys
 
-# Load the model
-filepath = './men_classifier'
-model = load_model(filepath, compile = True)
+women_categories = ['Dresses_Casual_Women', 'Evening_Dresses_Women',
+              'Jackets_Top_Sport_Women','Jens_Bottom_Casual_Women',
+              'Jumpsuits_Casual_Women', 'Pants_Bottom_Formal_Women',
+              'Shorts_Bottom_Sport_Women', 'Skirts_Bottom_Casual_Women',
+              'Skirts_Bottom_Formal_Women', 'Suits_Bottom_Formal_Women',
+              'Suits_Sport_Women', 'Tshirts_Top_Sport_Women',
+              'Tops_Casual_Women', 'Tops_Top_Formal_Women', 'Trousers_Bottom_Sport_Women']
 
-categories = ['Jackets_Top_Casual_Men', 'Jackets_Top_Formal_Men',
+men_categories = ['Jackets_Top_Casual_Men', 'Jackets_Top_Formal_Men',
               'Jackets_Top_Sport_Men','Shirts_Top_Casual_Men',
               'Shirts_Top_Formal_Men', 'Shorts_Bottom_Casual_Men',
               'Shorts_Bottom_Sport_Men', 'suits_Sport_Men',
               'Sweatshirt_Top_Casual_Men', 'T_Shirts_Top_Casual_Men',
               'T_Shirts_Top_Casual_Men', 'Trousers_Bottom_Casual_Men',
-              'Trousers_Bottom_Formal_Men', 'Trousers_Bottom_Sport_Men']
-
-
-# Get the input shape for the model layer
-input_shape = model.layers[0].input_shape
+              'Trousers_Bottom_Formal_Men', 'Trousers_Bottom_Sport_Men']             
 
 # Define the FastAPI app
 app = FastAPI()
@@ -34,7 +34,20 @@ class Prediction(BaseModel):
 
 # Define the /prediction route
 @app.post('/prediction/', response_model=Prediction)
-async def prediction_route(file: UploadFile = File(...)):
+async def prediction_route(gender: int,file: UploadFile = File(...)):
+
+# Load the model
+  if (gender == 2 ) :
+    filepath = './women_classifier/women_classifier.h5'
+    model = load_model(filepath, compile = True)
+    categories = women_categories
+  else :
+    filepath = './men_classifier'
+    model = load_model(filepath, compile = True)
+    categories = men_categories
+
+  # Get the input shape for the model layer
+  input_shape = model.layers[0].input_shape  
 
 # Ensure that this is an image
   if file.content_type.startswith('image/') is False:
@@ -76,8 +89,8 @@ async def prediction_route(file: UploadFile = File(...)):
     e = sys.exc_info()[1]
     raise HTTPException(status_code=500, detail=str(e))
     
-import nest_asyncio
-nest_asyncio.apply()
+# import nest_asyncio
+# nest_asyncio.apply()
 
-if __name__ == '__main__':
-     uvicorn.run(app, host='127.0.0.1', port=8000)
+# if __name__ == '__main__':
+#      uvicorn.run(app, host='127.0.0.1', port=8000)
